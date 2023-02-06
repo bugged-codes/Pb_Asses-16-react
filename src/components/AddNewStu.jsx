2090c26f4cf015dfe850f0c7104aa20118a77525
bugged-codes <vipul_49@outlook.com>
2023-02-06 19:15:25 +0530

    changed src indexes

--- src/components/AddNewStu.jsx
+++ src/components/AddNewStu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
+import './Component&Pages.css';
 
 const AddNewStu = () => {
 	return <div>AddNewStu</div>;
